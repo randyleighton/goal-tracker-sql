ca1b7186ab2a8f3264b2e158076b4be68e389d07
Randy Leighton <randyleighton@yahoo.com>
2014-08-17 16:55:44 +0000

    add games to class array, save and equality working.

--- lib/games.rb
+++ lib/games.rb
@@ -6,6 +6,24 @@ class Game
   
   def initialize(attributes)
     @game_date = attributes[:game_date]
+    
+  end
+
+  def self.all
+    @games = []
+    results = DB.exec("SELECT * FROM games;")
+    results.each do |result|
+      @games << Game.new({game_date: result['game_date']})
+    end
+    @games
+  end
+  
+  def save
+    DB.exec("INSERT INTO games (game_date) VALUES ('#{@game_date}');")
+  end
+
+  def ==(another_game)
+    self.game_date == another_game.game_date
   end
   
 end
--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -6,4 +6,22 @@ describe Game do
     expect(game).to be_an_instance_of Game
     expect(game.game_date).to eq '2014-08-06'
   end
+  
+  it 'starts with an empty array' do
+    expect(Game.all).to eq []
+  end
+  
+  it 'adds games to the database table' do
+    game = Game.new({:game_date => '2014-08-06'})
+    game.save
+    expect(Game.all).to eq [game]
+  end
+  
+  it 'makes similar objects the same' do
+    game1 = Game.new({:game_date => '2014-08-06'})
+    game2 = Game.new({:game_date => '2014-08-06'})
+    expect(game1).to eq game2
+  end
+  
+  
 end
