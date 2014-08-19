014ad98f89e9e21327cf683430cae213a0b6419c
Randy Leighton <randyleighton@yahoo.com>
2014-08-19 15:36:23 +0000

    add goal to player and get goals for player code working in players.rb, need to implement ui

--- lib/players.rb
+++ lib/players.rb
@@ -1,4 +1,5 @@
 require 'pg'
+require 'pry'
 
 class Player
   
@@ -28,6 +29,20 @@ class Player
   def save
     result = DB.exec("INSERT INTO players (name, number, team) VALUES ('#{@name}','#{@number}','#{@team}') RETURNING id;")
     @id = result.first['id'].to_i
+  end
+  
+  def add_goal(game_id)
+    DB.exec("INSERT INTO goals (game_id, player_id) VALUES (#{game_id}, #{@id});")
+  end
+
+  def get_goals
+    @goals = []
+    results = DB.exec("SELECT games.* FROM players JOIN goals ON (goals.player_id = players.id)
+                       JOIN games ON (goals.game_id = games.id) WHERE players.id = #{@id}")
+    results.each do |result|
+      @goals << Game.new({:id =>result['id'].to_i, :game_date=> result['game_date'],})
+    end
+    @goals
   end
   
   def ==(another_player)
--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pry'
 
 describe Player do
   it 'initializes an instance of Player and returns the characteristics' do
@@ -32,6 +33,18 @@ describe Player do
     player1.save
     expect(Player.find(player1.id)).to eq player1
   end  
+  
+  it 'adds a goal to a player' do
+    player1 = Player.new({id: 1, name: 'Gerard', number: 8, team: 'Liverpool'})
+    player1.save
+    game1 = Game.new({:game_date => '2014-08-06'})
+    game1.save
+    player1.add_goal(game1.id)
+    expect(player1.get_goals).to eq [game1]
+  end  
+  
+  
+  
   
   
   
